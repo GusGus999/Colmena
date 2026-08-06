1219710a326f1c158920ad82986ade8e07de0c50
gustavo <guzmanmartinezangelgustavo@gmail.com>
2026-08-06 15:38:38 -0600

    Frecuencia con INMP441 Y FFT

--- Panal/Panal.ino
+++ Panal/Panal.ino
@@ -1,3 +1,12 @@
+//Acustica -
+//Peso
+//Co2 -
+//Fotos 
+//Temperatura -
+//Temperatura Relativa 
+//Humedad -
+//Altura -
+
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
@@ -5,25 +14,47 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_SCD30.h>
+#include <Adafruit_BME280.h>
+#include <driver/i2s.h>
+#include "arduinoFFT.h"
 
-// --- CONFIGURACIÓN WI-FI ---
 const char* ssid = "Pichishouse_EXT";
 const char* password = "Pichi1970";
 
-// --- CONFIGURACIÓN OLED E I2C ---
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
 #define SCREEN_ADDRESS 0x3C
 
+#define I2C_SDA_PIN 21
+#define I2C_SCL_PIN 20
+#define I2S_SCK 40
+#define I2S_WS  41
+#define I2S_SD  42
+#define I2S_PORT I2S_NUM_0
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+const uint16_t muestras = 1024;           
+const double frecuencia_muestreo = 16000;  
+
+double vReal[muestras];
+double vImag[muestras];
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ArduinoFFT<double> FFT = ArduinoFFT<double>(vReal, vImag, muestras, frecuencia_muestreo);
 Adafruit_SCD30 scd30;
+Adafruit_BME280 bme;
 AsyncWebServer server(80);
 
 // Variables para almacenar la última lectura
 float tempActual = 0.0;
 float humActual = 0.0;
 float co2Actual = 0.0;
+float altitudActual = 0.0;
+float frecActual = 0;
+
+int32_t muestra_audio = 0;
+size_t bytes_leidos = 0;
 
 // Interfaz Web Almacenada en la memoria FLASH
 const char index_html[] PROGMEM = R"rawliteral(
@@ -62,6 +93,16 @@ const char index_html[] PROGMEM = R"rawliteral(
       <div class="value" id="hum">--</div>
       <div class="unit">%</div>
     </div>
+    <div class="card alt">
+      <div class="label">Altitud</div>
+      <div class="value" id="alt">--</div>
+      <div class="unit">m s.n.m.</div>
+    </div>
+    <div class="card frec">
+      <div class="label"Frecuencia</div>
+      <div class="value" id="frec">--</div>
+      <div class="unit">Hz</div>
+    </div>
   </div>
 <script>
   function obtenerDatos() {
@@ -71,6 +112,8 @@ const char index_html[] PROGMEM = R"rawliteral(
         document.getElementById("co2").innerText = Math.round(data.co2);
         document.getElementById("temp").innerText = data.temperatura.toFixed(1);
         document.getElementById("hum").innerText = data.humedad.toFixed(1);
+        document.getElementById("alt").innerText = data.altitud.toFixed(1);
+        document.getElementById("frec").innerText = data.frecuencia.toFixed(1);
       })
       .catch(err => console.error("Error obteniendo datos: ", err));
   }
@@ -82,6 +125,8 @@ const char index_html[] PROGMEM = R"rawliteral(
 
 void setup() {
   Serial.begin(115200);
+
+  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("Fallo OLED"));
@@ -120,6 +165,43 @@ void setup() {
     while (1) { delay(10); }
   }
 
+  //Iniciando bme
+  unsigned status = bme.begin(0x76); 
+  if (!status) {
+    status = bme.begin(0x77); // 0x76
+    if (!status) {
+      Serial.println("¡Error: No se encontró el BME280! Revisa cables o dirección.");
+      while (1) { delay(10); }
+    }
+  }
+
+  // Configuración del protocolo I2S para el micrófono
+  const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX), 
+    .sample_rate = (uint32_t)frecuencia_muestreo,      
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,      
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,       
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,          
+    .dma_buf_count = 8,                                
+    .dma_buf_len = 64,                                 
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0
+  };
+
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = I2S_PIN_NO_CHANGE, 
+    .data_in_num = I2S_SD              
+  };
+
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_PORT, &pin_config);
+  
+  Serial.println("Micrófono INMP441 listo. Escuchando...");
+
   // Configurar Servidor Asíncrono
   // Ruta principal (Servir página HTML)
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -140,14 +222,7 @@ void setup() {
   Serial.println("Servidor Web Asíncrono iniciado.");
 }
 
-void loop() {
-  if (scd30.dataReady()) {
-    
-    if (scd30.read()) {
-      co2Actual = scd30.CO2;
-      tempActual = scd30.temperature;
-      humActual = scd30.relative_humidity;
-
+void imprimirPantalla() {
       display.clearDisplay();
 
       display.setTextSize(2);
@@ -155,25 +230,75 @@ void loop() {
       display.print("Ambiente");
 
       display.setTextSize(1);
-      display.setCursor(0, 20);
+      display.setCursor(0, 16);
       display.print("CO2:  ");
-      display.print(scd30.CO2, 0);
+      display.print(co2Actual, 0);
       display.setTextSize(1);
       display.print(" ppm");
 
-      display.setCursor(0, 35);
+      display.setCursor(0, 26);
       display.print("Temp: ");
-      display.print(scd30.temperature, 1); 
+      display.print(tempActual, 1); 
       display.print(" C");
 
-      display.setCursor(0, 50);
+      display.setCursor(0, 36);
       display.print("Hum:  ");
-      display.print(scd30.relative_humidity, 1); 
+      display.print(humActual, 1); 
       display.print(" %");
 
+      display.setCursor(0, 46);
+      display.print("Alt:  ");
+      display.print(altitudActual, 1);
+      display.println(" m");
+
+      display.setCursor(0, 56);
+      display.print("Frec: ");
+      display.print(frecActual, 1);
+      display.println(" Hz");
+
       display.display();
+}
+
+void loop() {
+  if (scd30.dataReady()) {
+    if (scd30.read()) {
+      co2Actual = scd30.CO2;
+      tempActual = scd30.temperature;
+      humActual = scd30.relative_humidity;
     }
   }
 
+  altitudActual = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+  // Recolectar un paquetes de sonido
+  for (int i = 0; i < muestras; i++) {
+    i2s_read(I2S_PORT, &muestra_audio, sizeof(muestra_audio), &bytes_leidos, portMAX_DELAY);
+    
+    if (bytes_leidos > 0) {
+      vReal[i] = (double)(muestra_audio >> 14); 
+      vImag[i] = 0.0;
+    } else {
+      vReal[i] = 0.0;
+      vImag[i] = 0.0;
+    }
+  }
+
+  // Ejecutar la Transformada Rápida de Fourier
+  FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);
+  FFT.compute(FFTDirection::Forward);
+  FFT.complexToMagnitude();
+
+  // Obtener la Frecuencia Dominante
+  double frecuencia_dominante = FFT.majorPeak();
+
+  // Filtro básico de ruido
+  if (frecuencia_dominante > 20.0) { 
+    frecActual = frecuencia_dominante;
+  }
+
+  imprimirPantalla();
+
   delay(500);
 }
+
+
